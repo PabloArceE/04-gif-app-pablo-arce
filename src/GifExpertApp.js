94fd64d710a4369f64dc710cd1021aced6117ad6
Pablo Arce <arcepablo07@gmail.com>
2022-02-02 16:22:56 -0300

    AddCategories (#0202)

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,25 @@
 // rfcp react function component
 
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([]);
+
   return (
     <>
         <h1>Gif Expert App</h1>
+        <AddCategory setCategories={ setCategories }/>
         <hr/>
+
+        <ol>
+            {
+                categories.map( category => { 
+                    return <li key={category}> {category} </li>
+                })
+            }
+        </ol>
     </>
   )  
 }
