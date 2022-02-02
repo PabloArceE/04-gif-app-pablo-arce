7e218a8258cf66b1e0a56cd87ad3ebd407a5f894
Pablo Arce <arcepablo07@gmail.com>
2022-02-02 13:44:17 -0300

    inicio app (#0202)

---
+++ src/GifExpertApp.js
@@ -0,0 +1,12 @@
+// rfcp react function component
+
+import React from 'react';
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        <h1>Gif Expert App</h1>
+        <hr/>
+    </>
+  )  
+}
