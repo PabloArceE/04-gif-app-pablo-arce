8c6c53b2f5e0455d8407d5dd82f8b08e2228e594
Pablo Arce <arcepablo07@gmail.com>
2022-02-07 11:55:39 -0300

    fix borrado busquedas anteriores

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -12,7 +12,8 @@ export const AddCategory = ({ setCategories }) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         if(inputValue.trim().length > 2){
-            setCategories( cats => [inputValue, ...cats]);
+            // setCategories( cats => [inputValue, ...cats]);
+            setCategories( cats => [inputValue]);
             setInputValue('');
         }
     };
